9db32152919dd317aef26e9a7592555188bfb765
Kinscheck <sighburangel@gmail.com>
2021-01-24 17:21:42 -0500

    Initial commit

--- src/Complex.java
+++ src/Complex.java
@@ -41,34 +41,41 @@ public class Complex {
     }
 
     public static void main(String args[]) {
-        Complex c1 = new Complex(10,5);
+        Complex c1 = new Complex(-10,-5);
         Complex c2 = new Complex(20, 10);
-        Complex c3 = new Complex(3, 4);
+        Complex c3 = new Complex(-3, 0);
+        Complex c4 = new Complex(0 ,10);
 
         System.out.println("Print Complex");
         System.out.println(c1);
         System.out.println(c2);
         System.out.println(c3);
+        System.out.println(c4);
         System.out.println("\nAbs Method");
         System.out.println(c1.abs());
         System.out.println(c2.abs());
         System.out.println(c3.abs());
+        System.out.println(c4.abs());
         System.out.println("\nAdd Complex");
         System.out.println(c1.add(c2));
         System.out.println(c2.add(c1));
         System.out.println(c2.add(c3));
+        System.out.println(c4.add(c3));
         System.out.println("\nAdd Double");
         System.out.println(c1.add(10.0));
         System.out.println(c2.add(73.0));
         System.out.println(c3.add(7));
+        System.out.println(c4.add(2));
         System.out.println("\nMultiply Complex");
         System.out.println(c1.multiply(c2));
         System.out.println(c2.multiply(c1));
         System.out.println(c2.multiply(c3));
+        System.out.println(c4.multiply(c2));
         System.out.println("\nMultiply Double");
         System.out.println(c1.multiply(10));
         System.out.println(c2.multiply(5));
         System.out.println(c3.multiply(1500));
+        System.out.println(c4.multiply(2));
     }
 
 }
